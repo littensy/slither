1dc82e78009f97017340fc8215aa86ea6a7ef147
Richard <richerd.rdc@gmail.com>
2023-07-10 11:46:18 -0700

    refactor: remove unnecessary `useMemo`
    useMemo is not needed here because this component will only re-render when the dependencies change, so it will be re-calculated every render anyways.

--- src/client/app/components/world/world.tsx
+++ src/client/app/components/world/world.tsx
@@ -1,5 +1,5 @@
 import { useSelector } from "@rbxts/react-reflex";
-import Roact, { useMemo } from "@rbxts/roact";
+import Roact from "@rbxts/roact";
 import { selectSnakeIds } from "shared/store/snakes";
 import { Snake } from "../snake";
 import { useWorldView } from "./use-world-view";
@@ -8,9 +8,11 @@ export function World() {
 	const ids = useSelector(selectSnakeIds);
 	const { offset, scale } = useWorldView();
 
-	const snakes = useMemo(() => {
-		return ids.map((id) => <Snake key={`snake-${id}`} id={id} offset={offset} scale={scale} />);
-	}, [ids, offset, scale]);
-
-	return <>{snakes}</>;
+	return (
+		<>
+			{ids.map((id) => (
+				<Snake key={`snake-${id}`} id={id} offset={offset} scale={scale} />
+			))}
+		</>
+	);
 }
