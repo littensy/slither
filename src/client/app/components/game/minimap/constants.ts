5914931cd23753911e6fb1aa467c6f73c05001dd
Richard <richerd.rdc@gmail.com>
2023-07-22 14:31:00 -0700

    feat: adjust scaling on touch devices

---
+++ src/client/app/components/game/minimap/constants.ts
@@ -0,0 +1,1 @@
+export const MINIMUM_MINIMAP_REM = 10;
--- src/client/app/components/game/minimap/minimap.tsx
+++ src/client/app/components/game/minimap/minimap.tsx
@@ -3,62 +3,74 @@ import { Frame } from "client/app/common/frame";
 import { Group } from "client/app/common/group";
 import { Image } from "client/app/common/image";
 import { Shadow } from "client/app/common/shadow";
-import { useRem } from "client/app/hooks";
-import { BASE_REM } from "client/app/providers/rem-provider";
+import { useInputDevice, useRem } from "client/app/hooks";
+import { BASE_REM, RemProvider } from "client/app/providers/rem-provider";
 import { images } from "shared/assets";
 import { palette } from "shared/data/palette";
+import { MINIMUM_MINIMAP_REM } from "./constants";
 import { MinimapNodes } from "./minimap-nodes";
 
 export function Minimap() {
-	const rem = useRem();
+	const rem = useRem({ minimum: MINIMUM_MINIMAP_REM });
+	const device = useInputDevice();
+
+	const isTouch = device === "touch";
+	const isPhone = isTouch && rem(1) === MINIMUM_MINIMAP_REM;
+
+	// top-right corner on phone
+	// right-middle on larger touch devices
+	// bottom-right corner on desktop
+	const { anchorPoint, position } = isPhone
+		? { anchorPoint: new Vector2(1, 0), position: new UDim2(1, -rem(4), 0, rem(4)) }
+		: isTouch
+		? { anchorPoint: new Vector2(1, 0.5), position: new UDim2(1, -rem(4), 0.5, 0) }
+		: { anchorPoint: new Vector2(1, 1), position: new UDim2(1, -rem(4), 1, -rem(4)) };
 
 	return (
-		<Group
-			anchorPoint={new Vector2(1, 1)}
-			size={new UDim2(0, rem(10), 0, rem(10))}
-			position={new UDim2(1, rem(-3), 1, rem(-3))}
-		>
-			<Shadow
-				key="drop-shadow"
-				shadowColor={palette.black}
-				shadowSize={rem(5)}
-				shadowOffset={rem(1.5)}
-				shadowTransparency={0}
-			/>
-
-			<Frame
-				key="background"
-				backgroundColor={palette.white1}
-				cornerRadius={new UDim(1, 0)}
-				size={new UDim2(1, 0, 1, 0)}
-			>
-				<uigradient
-					Color={new ColorSequence(palette.crust, palette.mantle)}
-					Transparency={new NumberSequence(0.3, 0.1)}
-					Rotation={-45}
+		<RemProvider minimumRem={MINIMUM_MINIMAP_REM}>
+			<Group anchorPoint={anchorPoint} size={new UDim2(0, rem(10), 0, rem(10))} position={position}>
+				<Shadow
+					key="drop-shadow"
+					shadowColor={palette.black}
+					shadowSize={rem(5)}
+					shadowOffset={rem(1.5)}
+					shadowTransparency={0}
 				/>
-				<uistroke Color={palette.lavender} Transparency={0.9} Thickness={rem(0.25)} />
-			</Frame>
 
-			<Frame
-				key="inner-stroke"
-				backgroundTransparency={1}
-				cornerRadius={new UDim(1, 0)}
-				size={new UDim2(1, -2, 1, -2)}
-				position={new UDim2(0, 1, 0, 1)}
-			>
-				<uistroke Color={palette.text} Transparency={0.85} Thickness={rem(0.05)} />
-			</Frame>
+				<Frame
+					key="background"
+					backgroundColor={palette.white1}
+					cornerRadius={new UDim(1, 0)}
+					size={new UDim2(1, 0, 1, 0)}
+				>
+					<uigradient
+						Color={new ColorSequence(palette.crust, palette.mantle)}
+						Transparency={new NumberSequence(0.3, 0.1)}
+						Rotation={-45}
+					/>
+					<uistroke Color={palette.lavender} Transparency={0.9} Thickness={rem(0.25)} />
+				</Frame>
 
-			<Image
-				key="crosshair"
-				image={images.ui.map_crosshair}
-				anchorPoint={new Vector2(0.5, 0.5)}
-				size={new UDim2(0, rem(16 / BASE_REM), 0, rem(16 / BASE_REM))}
-				position={new UDim2(0.5, 0, 0.5, 0)}
-			/>
+				<Frame
+					key="inner-stroke"
+					backgroundTransparency={1}
+					cornerRadius={new UDim(1, 0)}
+					size={new UDim2(1, -2, 1, -2)}
+					position={new UDim2(0, 1, 0, 1)}
+				>
+					<uistroke Color={palette.text} Transparency={0.85} Thickness={rem(0.05)} />
+				</Frame>
 
-			<MinimapNodes />
-		</Group>
+				<Image
+					key="crosshair"
+					image={images.ui.map_crosshair}
+					anchorPoint={new Vector2(0.5, 0.5)}
+					size={new UDim2(0, rem(16 / BASE_REM), 0, rem(16 / BASE_REM))}
+					position={new UDim2(0.5, 0, 0.5, 0)}
+				/>
+
+				<MinimapNodes />
+			</Group>
+		</RemProvider>
 	);
 }
