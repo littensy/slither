fd0ffbac6bc08dd0541fda7e48739bfd39bc6fb2
Richard <richerd.rdc@gmail.com>
2023-07-23 21:42:23 -0700

    refactor: delete after-tests

--- src/server/test/helpers/after-tests.ts
+++
@@ -1,9 +0,0 @@
-import { RunService } from "@rbxts/services";
-import { setTimeout } from "@rbxts/set-timeout";
-
-/**
- * Allow TestService to initialize before running tests.
- */
-export function afterTests(callback: () => void) {
-	setTimeout(callback, RunService.IsStudio() ? 0.5 : 0);
-}
