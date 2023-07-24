b8ef26e921fb8122137dcf130b07d603a21fa870
Richard <richerd.rdc@gmail.com>
2023-07-23 19:31:42 -0700

    docs: describe reasoning for service module

--- src/server/world/services/index.ts
+++ src/server/world/services/index.ts
@@ -3,7 +3,20 @@ import { initCandyService } from "./candy-service";
 import { initCollisionService } from "./collision-service";
 import { initSnakeService } from "./snake-service";
 
-initBotService();
-initCandyService();
-initCollisionService();
-initSnakeService();
+/**
+ * Starts the services when imported. This is done to ensure only one instance
+ * of these services exist at one time, while having some control over when to
+ * start them.
+ *
+ * This is used by:
+ * - `server/world/world.server.ts` to run services on game start
+ * - `server/test/world/init.spec.ts` to make sure services run before tests
+ */
+function initServices() {
+	initBotService();
+	initCandyService();
+	initCollisionService();
+	initSnakeService();
+}
+
+initServices();
