2c33c9ac9a1e5f158a754b43b6cb6bec00d10cc2
Richard <richerd.rdc@gmail.com>
2023-07-28 11:48:42 -0700

    fix: update direction when humanoid moves

--- src/client/app/components/controller/use-touch-move.ts
+++ src/client/app/components/controller/use-touch-move.ts
@@ -1,5 +1,5 @@
 import { useCamera, useEventListener } from "@rbxts/pretty-react-hooks";
-import { useState } from "@rbxts/roact";
+import { useRef, useState } from "@rbxts/roact";
 import { Players, UserInputService } from "@rbxts/services";
 import { useCharacter } from "client/app/hooks";
 
@@ -10,10 +10,11 @@ import { useCharacter } from "client/app/hooks";
 export function useTouchMove() {
 	const camera = useCamera();
 	const character = useCharacter(Players.LocalPlayer);
+	const humanoid = character?.Humanoid;
 
+	const touchPosition = useRef(Vector2.zero);
 	const [touchInput, setTouchInput] = useState<InputObject>();
 	const [touchStart, setTouchStart] = useState(Vector2.zero);
-
 	const [direction, setDirection] = useState(Vector2.zero);
 	const [jumping, setJumping] = useState(false);
 
@@ -24,31 +25,38 @@ export function useTouchMove() {
 
 	// Only set direction if this input started on the left side
 	// of the screen
-	const isLeftSide = (input: InputObject) => {
-		return input.Position.X < camera.ViewportSize.X / 2;
+	const getSide = (input: InputObject): "left" | "right" => {
+		return input.Position.X < camera.ViewportSize.X / 2 ? "left" : "right";
 	};
 
 	useEventListener(UserInputService.TouchStarted, (input) => {
-		if (!touchInput && isLeftSide(input)) {
+		if (getSide(input) === "left") {
+			const position = new Vector2(input.Position.X, input.Position.Y);
 			setTouchInput(input);
-			setTouchStart(new Vector2(input.Position.X, input.Position.Y));
+			setTouchStart(position);
+			touchPosition.current = position;
 		}
 	});
 
 	useEventListener(UserInputService.TouchEnded, (input) => {
 		if (input === touchInput) {
 			setTouchInput(undefined);
-			setTouchStart(Vector2.zero);
-			setDirection(Vector2.zero);
 		}
 
-		setJumping(false);
+		if (getSide(input) === "right") {
+			setJumping(false);
+		}
 	});
 
 	useEventListener(UserInputService.TouchMoved, (input) => {
-		if (input === touchInput && isMoving()) {
-			const position = new Vector2(input.Position.X, input.Position.Y);
-			const delta = position.sub(touchStart);
+		if (input === touchInput) {
+			touchPosition.current = new Vector2(input.Position.X, input.Position.Y);
+		}
+	});
+
+	useEventListener(humanoid?.GetPropertyChangedSignal("MoveDirection"), () => {
+		if (humanoid && humanoid.MoveDirection !== Vector3.zero) {
+			const delta = touchPosition.current.sub(touchStart);
 			const direction = delta !== Vector2.zero ? delta.Unit : Vector2.zero;
 			setDirection(direction);
 		}
