497ccf444d3fb730307b7486b39923921d51ce55
Richard <richerd.rdc@gmail.com>
2023-08-04 20:59:45 -0700

    refactor: use hydrate mount option

--- src/client/app/app.client.tsx
+++ src/client/app/app.client.tsx
@@ -1,21 +1,19 @@
 import { createRoot } from "@rbxts/react-roblox";
-import Roact, { Portal, StrictMode } from "@rbxts/roact";
+import Roact, { StrictMode } from "@rbxts/roact";
 import { Players } from "@rbxts/services";
 import { App } from "./components/app";
 import { RootProvider } from "./providers/root-provider";
 import { profileAllComponents } from "./utils/profiler";
 
-const root = createRoot(new Instance("Folder"));
 const target = Players.LocalPlayer.WaitForChild("PlayerGui");
+const root = createRoot(target, { hydrate: true });
 
 profileAllComponents();
 
 root.render(
 	<StrictMode>
 		<RootProvider>
-			<Portal target={target}>
-				<App />
-			</Portal>
+			<App />
 		</RootProvider>
 	</StrictMode>,
 );
