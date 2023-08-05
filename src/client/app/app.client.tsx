8e784e4d20278c2a4331554b251b30e97597903a
Richard <richerd.rdc@gmail.com>
2023-08-04 21:56:15 -0700

    revert: refactor: use hydrate mount option
    This reverts commit 497ccf444d3fb730307b7486b39923921d51ce55.
    
    The implementation for this in React Luau is actually incomplete.

--- src/client/app/app.client.tsx
+++ src/client/app/app.client.tsx
@@ -1,19 +1,21 @@
 import { createRoot } from "@rbxts/react-roblox";
-import Roact, { StrictMode } from "@rbxts/roact";
+import Roact, { Portal, StrictMode } from "@rbxts/roact";
 import { Players } from "@rbxts/services";
 import { App } from "./components/app";
 import { RootProvider } from "./providers/root-provider";
 import { profileAllComponents } from "./utils/profiler";
 
+const root = createRoot(new Instance("Folder"));
 const target = Players.LocalPlayer.WaitForChild("PlayerGui");
-const root = createRoot(target, { hydrate: true });
 
 profileAllComponents();
 
 root.render(
 	<StrictMode>
 		<RootProvider>
-			<App />
+			<Portal target={target}>
+				<App />
+			</Portal>
 		</RootProvider>
 	</StrictMode>,
 );
