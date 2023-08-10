50594b274ec1834c88cbd8326ff480c35d35b745
Richard <richerd.rdc@gmail.com>
2023-08-10 15:11:27 -0700

    refactor: remove redundant variables

--- src/client/app/components/game/minimap/minimap-tracer.tsx
+++ src/client/app/components/game/minimap/minimap-tracer.tsx
@@ -10,21 +10,16 @@ interface MinimapTracerProps {
 
 export function MinimapTracer({ from, to }: MinimapTracerProps) {
 	const rem = useRem();
-
 	const center = from.add(to).div(2);
 	const length = from.sub(to).Magnitude;
-
-	const position = new UDim2(center.X, 0, center.Y, 0);
-	const size = new UDim2(length, rem(0.2), 0, rem(0.2));
-	const rotation = math.deg(math.atan2(to.Y - from.Y, to.X - from.X));
 
 	return (
 		<Frame
 			backgroundColor={palette.lavender}
 			anchorPoint={new Vector2(0.5, 0.5)}
-			size={size}
-			position={position}
-			rotation={rotation}
+			size={new UDim2(center.X, 0, center.Y, 0)}
+			position={new UDim2(length, rem(0.2), 0, rem(0.2))}
+			rotation={math.deg(math.atan2(to.Y - from.Y, to.X - from.X))}
 		/>
 	);
 }
