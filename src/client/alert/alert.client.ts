1f83eba555ed046c197a870d2a14500fd6677fab
littensy <56808540+littensy@users.noreply.github.com>
2023-08-19 12:49:48 -0700

    feat: add scoped alerts (#51)
    * feat: add scoped alerts
    
    * fix: dismiss alert based on visible index

--- src/client/alert/alert.client.ts
+++ src/client/alert/alert.client.ts
@@ -1,21 +1,13 @@
 import { store } from "client/store";
-import { Alert } from "client/store/alert";
 import { palette } from "shared/data/palette";
 import { remotes } from "shared/remotes";
 import { selectLocalSnakeRanking } from "shared/store/snakes";
 
-import { dismissAlert, sendAlert } from ".";
+import { sendAlert } from ".";
 
-const FIRST_PLACE = 'Congratulations, you are in <font color="#fff">first place</font>!';
-const SECOND_PLACE = 'Congratulations, you are in <font color="#fff">second place</font>!';
-const THIRD_PLACE = 'Congratulations, you are in <font color="#fff">third place</font>!';
-
-let previousAlertId = 0;
-
-function sendAlertLimited(alert: Partial<Alert>) {
-	dismissAlert(previousAlertId);
-	previousAlertId = sendAlert(alert);
-}
+const FIRST_PLACE = 'Congratulations! You are in <font color="#fff">first place</font>.';
+const SECOND_PLACE = 'Congratulations! You are in <font color="#fff">second place</font>.';
+const THIRD_PLACE = 'Congratulations! You are in <font color="#fff">third place</font>.';
 
 remotes.client.alert.connect((alert) => {
 	sendAlert(alert);
@@ -23,21 +15,24 @@ remotes.client.alert.connect((alert) => {
 
 store.subscribe(selectLocalSnakeRanking, (ranking) => {
 	if (ranking === 1) {
-		sendAlertLimited({
+		sendAlert({
+			scope: "ranking",
 			emoji: "🏆",
 			color: palette.yellow,
 			colorSecondary: palette.peach,
 			message: FIRST_PLACE,
 		});
 	} else if (ranking === 2) {
-		sendAlertLimited({
+		sendAlert({
+			scope: "ranking",
 			emoji: "🥈",
 			color: palette.sapphire,
 			colorSecondary: palette.blue,
 			message: SECOND_PLACE,
 		});
 	} else if (ranking === 3) {
-		sendAlertLimited({
+		sendAlert({
+			scope: "ranking",
 			emoji: "🥉",
 			color: palette.maroon,
 			colorSecondary: palette.red,
--- src/client/store/alert/alert-selectors.ts
+++ src/client/store/alert/alert-selectors.ts
@@ -1,5 +1,17 @@
+import { createSelector } from "@rbxts/reflex";
+
 import { RootState } from "..";
 
 export const selectAlerts = (state: RootState) => {
 	return state.alert.alerts;
+};
+
+export const selectAlertsVisible = createSelector(selectAlerts, (alerts) => {
+	return alerts.filter((alert) => alert.visible);
+});
+
+export const selectAlertIndex = (id: number) => {
+	return createSelector(selectAlertsVisible, (alerts) => {
+		return alerts.findIndex((alert) => alert.id === id);
+	});
 };
