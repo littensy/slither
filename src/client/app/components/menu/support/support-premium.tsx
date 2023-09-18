fde404e41bd323ff280a70d50701564cca249f31
Inversion <neoinversion@outlook.com>
2023-09-17 19:22:56 -0700

    refactor: fix button element keys

--- src/client/app/components/menu/support/support-premium.tsx
+++ src/client/app/components/menu/support/support-premium.tsx
@@ -24,13 +24,18 @@ export function SupportPremium() {
 	return (
 		<AwesomeButton
 			onClick={promptPremiumPurchase}
-			key="prompt-button"
 			anchorPoint={new Vector2(1, 1)}
 			position={new UDim2(1, rem(-3), 1, rem(-3))}
 			size={new UDim2(0, rem(4), 0, rem(4))}
 			overlayGradient={new ColorSequence(palette.red, palette.blue)}
 		>
-			<Text position={new UDim2(0.5, 0, 0.5, 0)} textSize={rem(2)} textColor={palette.black} text="\u{E001} " />
+			<Text
+				key="premium-icon"
+				position={new UDim2(0.5, 0, 0.5, 0)}
+				textSize={rem(2)}
+				textColor={palette.black}
+				text="\u{E001} "
+			/>
 		</AwesomeButton>
 	);
 }
