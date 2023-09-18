a70fb5d758c8ee5beb79b94badac0fb50771625b
Inversion <neoinversion@outlook.com>
2023-09-17 18:47:55 -0700

    feat: add premium purchase prompt

---
+++ src/client/app/components/menu/support/support-premium.tsx
@@ -0,0 +1,31 @@
+import Roact from "@rbxts/roact";
+import { MarketplaceService, Players } from "@rbxts/services";
+import { sendAlert } from "client/alert";
+import { AwesomeButton } from "client/app/common/awesome-button";
+import { Text } from "client/app/common/text";
+import { useRem } from "client/app/hooks";
+import { palette } from "shared/data/palette";
+
+export function SupportPremium() {
+	const rem = useRem();
+	const promptPremiumPurchase = async () => {
+		if (Players.LocalPlayer.MembershipType === Enum.MembershipType.Premium) {
+			sendAlert({});
+		} else {
+			MarketplaceService.PromptPremiumPurchase(Players.LocalPlayer);
+		}
+	};
+
+	return (
+		<AwesomeButton
+			onClick={promptPremiumPurchase}
+			key="prompt-button"
+			anchorPoint={new Vector2(1, 1)}
+			position={new UDim2(1, rem(-3), 1, rem(-3))}
+			size={new UDim2(0, rem(4), 0, rem(4))}
+			overlayGradient={new ColorSequence(palette.red, palette.blue)}
+		>
+			<Text position={new UDim2(0.5, 0, 0.5, 0)} textSize={rem(2)} textColor={palette.black} text="\u{E001} " />
+		</AwesomeButton>
+	);
+}
