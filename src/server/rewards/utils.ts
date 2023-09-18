070bcf8f5d612a0a9d0bb74f4598c2b7a9ada3aa
Inversion <40722286+NeoInversion@users.noreply.github.com>
2023-09-17 19:52:38 -0700

    fix: displayed reward and purchase money value (#76)

--- src/server/rewards/utils.ts
+++ src/server/rewards/utils.ts
@@ -3,10 +3,11 @@ import { PREMIUM_BENEFIT } from "shared/constants";
 
 export function grantMoney(player: Player, amount: number) {
 	if (player.MembershipType === Enum.MembershipType.Premium) {
-		amount *= PREMIUM_BENEFIT;
+		amount = math.round(amount * PREMIUM_BENEFIT);
+	} else {
+		amount = math.round(amount);
 	}
-
-	store.givePlayerBalance(player.Name, math.floor(amount));
+	store.givePlayerBalance(player.Name, amount);
 
 	return amount;
 }
