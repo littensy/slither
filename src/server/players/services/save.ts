abe5779e66970f3873d15467689059930c93dfd7
littensy <56808540+littensy@users.noreply.github.com>
2023-09-18 18:54:30 -0700

    refactor: use lapis for data storage (#77)

--- src/server/players/services/save.ts
+++ src/server/players/services/save.ts
@@ -1,12 +1,15 @@
-import { DataStoreService, Players } from "@rbxts/services";
-import { setInterval } from "@rbxts/set-timeout";
+import { createCollection } from "@rbxts/lapis";
+import { Players } from "@rbxts/services";
 import { store } from "server/store";
 import { palette } from "shared/data/palette";
 import { remotes } from "shared/remotes";
 import { defaultPlayerSave, playerSaveSchema, selectPlayerSave } from "shared/store/saves";
 import { promisePlayerDisconnected } from "shared/utils/player-utils";
 
-const dataStore = DataStoreService.GetDataStore("saves", "v1");
+const collection = createCollection("players", {
+	defaultData: defaultPlayerSave,
+	validate: playerSaveSchema,
+});
 
 export async function initSaveService() {
 	Players.PlayerAdded.Connect(loadPlayerSave);
@@ -18,69 +21,42 @@ export async function initSaveService() {
 
 async function loadPlayerSave(player: Player) {
 	try {
-		const [save = defaultPlayerSave] = dataStore.GetAsync(`${player.UserId}`);
+		const document = await collection.load(`${player.UserId}`);
 
 		if (!player.IsDescendantOf(Players)) {
-			return;
+			return document.close();
 		}
 
-		if (!playerSaveSchema(save)) {
-			warn(`Invalid save data for ${player.Name}, merging with default save`);
-			return fallbackPlayerSave(player, save);
-		}
-
-		const disconnect = setInterval(() => {
-			saveToDataStore(player);
-		}, 60);
+		const disconnect = store.subscribe(selectPlayerSave(player.Name), (newSave) => {
+			if (newSave) {
+				document.write(newSave);
+			}
+		});
 
 		promisePlayerDisconnected(player).then(() => {
 			store.deletePlayerSave(player.Name);
 			disconnect();
-			saveToDataStore(player);
+			document.close();
 		});
 
-		store.setPlayerSave(player.Name, save);
+		store.setPlayerSave(player.Name, document.read());
 	} catch (e) {
 		warn(`Failed to load data for ${player.Name}: ${e}`);
 		fallbackPlayerSave(player);
 	}
 }
 
-async function fallbackPlayerSave(player: Player, save?: unknown) {
-	store.setPlayerSave(player.Name, {
-		...defaultPlayerSave,
-		...(typeIs(save, "table") && save),
-	});
-
+async function fallbackPlayerSave(player: Player) {
 	promisePlayerDisconnected(player).then(() => {
 		store.deletePlayerSave(player.Name);
 	});
 
-	remotes.client.alert.fire(player, {
-		emoji: "✅",
-		color: palette.green,
-		message: "Your data is safe! But we're having trouble loading it.",
-		duration: 10,
-	});
+	store.setPlayerSave(player.Name, defaultPlayerSave);
 
 	remotes.client.alert.fire(player, {
 		emoji: "🚨",
 		color: palette.red,
-		message: "Roblox may be experiencing issues. Sorry for the inconvenience!",
+		message: "Roblox may be having issues with your save. Sorry for the inconvenience!",
 		duration: 10,
 	});
-}
-
-async function saveToDataStore(player: Player) {
-	const save = store.getState(selectPlayerSave(player.Name));
-
-	if (!save) {
-		return;
-	}
-
-	try {
-		dataStore.SetAsync(`${player.UserId}`, save, [player.UserId]);
-	} catch (e) {
-		warn(`Failed to auto-save data for ${player.Name}: ${e}`);
-	}
 }
