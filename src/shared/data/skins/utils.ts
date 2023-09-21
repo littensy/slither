8d708f3c906a8af4ffeee2420b06548b8f32c7e8
NeoInversion <neoinversion@outlook.com>
2023-09-21 00:35:59 -0700

    fix: skin color blending
    Co-authored-by: littensy <littensy@users.noreply.github.com>

--- src/shared/data/skins/utils.ts
+++ src/shared/data/skins/utils.ts
@@ -1,6 +1,19 @@
-import { map } from "shared/utils/math-utils";
+import { lerp, lerpRadians, map } from "shared/utils/math-utils";
 
-export function blendColorSequence(colors: Color3[], length: number, looped = false) {
+const TAU = 2 * math.pi;
+
+function lerpHue(from: number, to: number, alpha: number) {
+	return (lerpRadians(from * TAU, to * TAU, alpha) / TAU) % 1;
+}
+
+function lerpHSV(from: Color3, to: Color3, alpha: number) {
+	const [fromH, fromS, fromV] = Color3.toHSV(from);
+	const [toH, toS, toV] = Color3.toHSV(to);
+
+	return Color3.fromHSV(lerpHue(fromH, toH, alpha), lerp(fromS, toS, alpha), lerp(fromV, toV, alpha));
+}
+
+export function blendColorSequence(colors: Color3[], length: number, looped = true): Color3[] {
 	if (looped) {
 		colors = [...colors, colors[0]];
 	}
@@ -15,7 +28,19 @@ export function blendColorSequence(colors: Color3[], length: number, looped = fa
 		const from = colors[indexInArray];
 		const to = colors[(indexInArray + 1) % colorCount];
 
-		pattern.push(from.Lerp(to, indexScaled % 1));
+		pattern[index] = lerpHSV(from, to, indexScaled % 1);
+	}
+
+	return pattern;
+}
+
+export function duplicateSequence<T extends defined>(sequence: T[], repeatCount: number): T[] {
+	const pattern: T[] = [];
+
+	for (const _ of $range(0, repeatCount - 1)) {
+		for (const value of sequence) {
+			pattern.push(value);
+		}
 	}
 
 	return pattern;
