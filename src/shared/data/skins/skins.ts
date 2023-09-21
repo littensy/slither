9734b35fdc1610290f1d5561854d3211d98c28e7
Inversion <neoinversion@outlook.com>
2023-09-20 19:45:46 -0700

    feat: add more skins
    Co-authored-by: littensy <littensy@users.noreply.github.com>

--- src/shared/data/skins/skins.ts
+++ src/shared/data/skins/skins.ts
@@ -1,6 +1,7 @@
-import { accentList, accents } from "../palette";
+import { accentList, accents, palette } from "../palette";
 import { SnakeSkin } from "../skins";
 import { defaultSnakeSkin } from "./types";
+import { blendColorSequence } from "./utils";
 
 export const baseSnakeSkins: readonly SnakeSkin[] = accentList.map((id) => {
 	return { ...defaultSnakeSkin, id, tint: [accents[id]] };
@@ -11,19 +12,73 @@ export const snakeSkins: readonly SnakeSkin[] = [
 
 	{
 		...defaultSnakeSkin,
+		id: "peppermint",
+		price: 125,
+		tint: [palette.red, palette.red, palette.white, palette.white],
+	},
+
+	{
+		...defaultSnakeSkin,
+		id: "japan",
+		price: 125,
+		tint: [palette.white, palette.white, palette.red],
+	},
+
+	{
+		...defaultSnakeSkin,
+		id: "honeybee",
+		price: 150,
+		tint: [palette.mantle, palette.mantle, palette.yellow],
+	},
+
+	{
+		...defaultSnakeSkin,
+		id: "conspiracy",
+		price: 175,
+		tint: blendColorSequence([palette.mantle, palette.mantle, palette.yellow], 8, false),
+	},
+
+	{
+		...defaultSnakeSkin,
+		id: "candycorn",
+		price: 150,
+		tint: [palette.peach, palette.peach, palette.yellow, palette.yellow, palette.white],
+	},
+
+	{
+		...defaultSnakeSkin,
+		id: "berries-and-cherries",
+		price: 250,
+		tint: blendColorSequence([palette.red, palette.blue], 8, true),
+	},
+
+	{
+		...defaultSnakeSkin,
+		id: "hot-cocoa",
+		price: 250,
+		tint: blendColorSequence([Color3.fromRGB(99, 74, 61), Color3.fromRGB(59, 44, 36)], 8, true),
+		primary: Color3.fromRGB(125, 94, 76),
+		secondary: Color3.fromRGB(92, 69, 56),
+	},
+
+	{
+		...defaultSnakeSkin,
 		id: "rainbow",
-		price: 45,
-		tint: [
-			accents.red,
-			accents.peach,
-			accents.yellow,
-			accents.green,
-			accents.teal,
-			accents.sky,
-			accents.sapphire,
-			accents.blue,
-			accents.lavender,
-		],
+		price: 300,
+		tint: blendColorSequence(
+			[
+				accents.red,
+				accents.peach,
+				accents.yellow,
+				accents.green,
+				accents.teal,
+				accents.sky,
+				accents.sapphire,
+				accents.blue,
+				accents.lavender,
+			],
+			16,
+		),
 		primary: Color3.fromRGB(186, 51, 84),
 		secondary: Color3.fromRGB(122, 51, 107),
 	},
