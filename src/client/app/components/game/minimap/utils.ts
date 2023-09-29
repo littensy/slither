0f637f28c1cd905ae56adbd79858f64f65f7cd61
Richard <richerd.rdc@gmail.com>
2023-09-28 20:50:53 -0700

    feat: highlight players in minimap

--- src/client/app/components/game/minimap/utils.ts
+++ src/client/app/components/game/minimap/utils.ts
@@ -1,4 +1,6 @@
-import { map } from "@rbxts/pretty-react-hooks";
+import { map, useInterval } from "@rbxts/pretty-react-hooks";
+import { useState } from "@rbxts/roact";
+import { Players } from "@rbxts/services";
 import { useRem } from "client/app/hooks";
 import { WORLD_BOUNDS } from "shared/constants";
 
@@ -6,6 +8,29 @@ export const MINIMUM_MINIMAP_REM = 10;
 
 export function useMinimapRem() {
 	return useRem({ minimum: MINIMUM_MINIMAP_REM });
+}
+
+export function useFriendsInServer() {
+	const [friends, setFriends] = useState<string[]>([]);
+
+	const updateFriendship = async (player: Player) => {
+		if (!friends.includes(player.Name) && player.IsFriendsWith(Players.LocalPlayer.UserId)) {
+			setFriends((friends) => [...friends, player.Name]);
+		}
+	};
+
+	const updateFriends = async () => {
+		const processes = Players.GetPlayers().map(updateFriendship);
+		return Promise.allSettled(processes);
+	};
+
+	useInterval(updateFriends, 10, { immediate: true });
+
+	return friends;
+}
+
+export function isValidPlayer(name: string) {
+	return Players.FindFirstChild(name)?.IsA("Player") || false;
 }
 
 export function normalizeToWorldBounds(vector: Vector2) {
