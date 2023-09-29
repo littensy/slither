7f315c19159ab973eef7a64134cfd111807269fa
Richard <56808540+littensy@users.noreply.github.com>
2023-09-29 01:05:41 -0700

    fix: decrease map size (#125)

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -5,7 +5,7 @@ import { $NODE_ENV } from "rbxts-transform-env";
 // during a game or when purchasing a product from the shop.
 export const PREMIUM_BENEFIT = 1.2;
 
-export const WORLD_BOUNDS = 196;
+export const WORLD_BOUNDS = 128;
 export const WORLD_TICK = 1 / 20;
 
 export const SNAKE_SPEED = 6;
