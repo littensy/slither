3cba9129c5cc17db9757a7bf5d385886ee0143d7
phantomfills <phantomfills@outlook.com>
2024-06-09 11:13:49 +0100

    feat: add eliminations + cash to scoreboard

--- src/server/players/services/scoreboard.ts
+++ src/server/players/services/scoreboard.ts
@@ -1,4 +1,5 @@
 import { store } from "server/store";
+import { selectPlayerBalance } from "shared/store/saves";
 import { selectSnakeById } from "shared/store/snakes";
 import { onPlayerAdded, promisePlayerDisconnected } from "shared/utils/player-utils";
 
@@ -8,16 +9,30 @@ export async function initScoreboardService() {
 		stats.Name = "leaderstats";
 		stats.Parent = player;
 
+		const knockouts = new Instance("IntValue");
+		knockouts.Name = "KOs";
+		knockouts.Parent = stats;
+
 		const score = new Instance("IntValue");
 		score.Name = "Score";
 		score.Parent = stats;
 
-		const unsubscribe = store.subscribe(selectSnakeById(player.Name), (snake) => {
+		const cash = new Instance("IntValue");
+		cash.Name = "Cash";
+		cash.Parent = stats;
+
+		const unsubscribeFromScoreAndKnockoutsUpdate = store.subscribe(selectSnakeById(player.Name), (snake) => {
 			score.Value = snake ? snake.score : 0;
+			knockouts.Value = snake ? snake.eliminations : 0;
+		});
+
+		const unsubscribeFromCashUpdate = store.subscribe(selectPlayerBalance(player.Name), (balance) => {
+			cash.Value = balance ?? 0;
 		});
 
 		promisePlayerDisconnected(player).then(() => {
-			unsubscribe();
+			unsubscribeFromScoreAndKnockoutsUpdate();
+			unsubscribeFromCashUpdate();
 		});
 	});
 }
